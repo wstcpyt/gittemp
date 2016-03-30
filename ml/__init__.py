1c636d12ac070ac74001322563c0a0b281bf3804
Yutong <wstcpyt@gmail.com>
2016-03-30 16:48:38 -0400

    city_model

--- ml/__init__.py
+++ ml/__init__.py
@@ -8,6 +8,17 @@ from .data import test_json
 from sklearn.externals import joblib
 import sklearn as sk
 
+# City Model Class
+class City_model():
+    def __init__(self):
+        pass
+    def fit(self, city_df):
+        self.city_grouped = df.groupby('city').agg({'stars':np.mean}).reset_index()
+    def predict(self, name):
+        return float(self.city_grouped[self.city_grouped.city==name].stars)
+citymodel = joblib.load("./ml/model/city/city_model.pkl")
+
+
 
 def pick(whitelist, dicts):
     return [toolz.keyfilter(lambda k: k in whitelist, d)
@@ -21,19 +32,8 @@ def exclude(blacklist, dicts):
 @fellow.batch(name="ml.city_model")
 @typecheck.test_cases(record=pick({"city"}, test_json))
 @typecheck.returns("number")
-
-# City Model Class
-class City_model():
-    def __init__(self):
-        pass
-    def fit(self, city_df):
-        self.city_grouped = df.groupby('city').agg({'stars':np.mean}).reset_index()
-    def predict(self, name):
-        return float(self.city_grouped[self.city_grouped.city==name].stars)
-    
 def city_model(record):
-    model = joblib.load("./ml/model/city/city_model.pkl")
-    return model.predict(record)
+    return citymodel.predict(record)
 
 
 @fellow.batch(name="ml.lat_long_model")
