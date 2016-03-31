be8c00e9e1103e077f119257ed6d750d97533fe1
Yutong <wstcpyt@gmail.com>
2016-03-30 20:36:56 -0400

    city

--- ml/__init__.py
+++ ml/__init__.py
@@ -24,7 +24,7 @@ def exclude(blacklist, dicts):
 @typecheck.test_cases(record=pick({"city"}, test_json))
 @typecheck.returns("number")
 def city_model(record):
-    print record
+    print record['city']
     return citymodel.predict(record)
 
 
