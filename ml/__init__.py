742dcf8b665da70d7f676c6d9f04c47385ebee5d
Yutong <wstcpyt@gmail.com>
2016-04-01 11:05:17 -0400

    using transformer

--- ml/__init__.py
+++ ml/__init__.py
@@ -65,7 +65,7 @@ def category_model(record):
 def attribute_knn_model(record):
     print record
     transformed_x = attribute_transform.transform(record["attributes"])
-    print attribute_transform.transform(record["attributes"])
+    print attribute_transform.transform(record["attributes"])[0]
     return attribute_model.predict(transformed_x)[0]
 
 
