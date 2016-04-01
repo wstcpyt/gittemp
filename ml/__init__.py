17f1b44ca1f5269aad92efec78a9f52f5d2d227e
Yutong <wstcpyt@gmail.com>
2016-04-01 11:09:33 -0400

    using transformer

--- ml/__init__.py
+++ ml/__init__.py
@@ -66,7 +66,7 @@ def attribute_knn_model(record):
     print record
     transformed_x = attribute_transform.transform(record["attributes"])
     print attribute_model.predict(record["attributes"])[0]
-    return attribute_model.predict(transformed_x)[0]
+    return 0
 
 
 @fellow.batch(name="ml.full_model")
